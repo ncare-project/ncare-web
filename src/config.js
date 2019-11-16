fcdffd810ece9cd124eacb78aad3c87becaf3b43
Shadi <floydanthebeast@gmail.com>
2019-11-16 17:01:04 +0300

    Init commit, add libraries, add basic code

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const config = {
+    server_base_url: '45.143.136.60:5000'
+}
+
+module.exports = config 
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const io = require('socket.io-client')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+ReactDOM.render(<App/>, document.querySelector('.root'))
