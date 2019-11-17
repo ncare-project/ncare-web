2797ef5f21404c5d83a82343c3935f96da06c6c1
Shadi <floydanthebeast@gmail.com>
2019-11-17 08:34:56 +0300

    Fix reports map component

--- src/components/ReportsMap/ReportsMap.js
+++ src/components/ReportsMap/ReportsMap.js
@@ -7,14 +7,61 @@ export default class ReportsMap extends Component {
         super(props)
 
         this.state = {
-            markers: [[55.7, 37.6]]
+            selectedPoint: null,
+            markers: [
+                {
+                    hint: 'testdsgdsgds',
+                    coordinates: [55.7, 37.6],
+                    radius: 10000
+                }
+            ]
+        }
+
+        this.handleInfoEdit = this.handleInfoEdit.bind(this)
+        this.getZones = this.getZones.bind(this)
+        
+        this.getZones()
+    }
+
+    getZones() {
+        window.socket.emit('orgs:get_zones')
+
+        window.socket.on('orgs:get_zones', data => {
+            console.log(data)
+            if (!data.res) { 
+                // Положительный ответ от сервера
+            }
+        })
+    }
+
+    handleInfoEdit() {
+        if (this.state.selectedPoint) {
+            const selectedPoint = this.state.selectedPoint
+
+            window.socket.emit('orgs:create_zone', {
+                name: this.state.markers[selectedPoint].hint,
+                coordinates: this.state.markers[selectedPoint].coordinates,
+                radius: this.state.markers[selectedPoint].radius
+            })
+
+            window.socket.on('orgs:create_zone', data => {
+                if (!data.res) { 
+                    // Положительный ответ от сервера
+                    
+
+                }
+            })
         }
     }
 
     render() {
         return (
             <div style={{height: '800px'}}>
-                <MenuBar handleUserExit={this.props.handleUserExit} sideMenu isSignedIn={this.props.isSignedIn}/>
+                <MenuBar handleUserExit={this.props.handleUserExit} sideMenu isSignedIn={this.props.isSignedIn}>
+                    <input ref={node => this.nameInput = node}></input>
+                    <input ref={node => this.radiusInput = node}></input>
+                    <button onClick={this.handleInfoEdit}>Submit</button>
+                </MenuBar>
                 {/* <Map center={[55.7007, 37.6]} zoom={10} onClick={data => {
                     this.setState(prevState => ({
                         markers: prevState.markers.concat([data.latLng])
@@ -26,22 +73,42 @@ export default class ReportsMap extends Component {
                 </Map> */}
                 <YMaps>
                     <Map width={'100%'} height={'100vh'} 
-                        defaultState={{ center: [55.75, 37.57], zoom: 9 }}>
-                            <GeoObject 
-                                geometry={{
-                                    type: 'Circle',
-                                    coordinates: [55.7, 37.6],
-                                    radius: 30000
-                                }}
-                            />
-
+                        defaultState={{ center: [55.75, 37.57], zoom: 9 }}
+                        onClick={e => this.setState(prevState => ({
+                            selectedPoint: prevState.markers.length,
+                            markers: prevState.markers.concat({
+                                hint: '',
+                                coordinates: e.get('coords'),
+                                radius: 0
+                            })
+                        }))}>
                             {this.state.markers.map((marker, index) => (
-                                <GeoObject 
-                                    geometry={{
-                                        type: 'Point',
-                                        coordinates: marker
-                                    }}
-                                />
+                                <>
+                                    <GeoObject
+                                        onClick={() => {
+                                            this.setState({
+                                                selectedPoint: index
+                                            })
+                                        }}
+                                        geometry={{
+                                            type: 'Point',
+                                            coordinates: marker.coordinates
+                                        }}
+                                        properties={{
+                                            iconContent: marker.hint.slice(0, 10)
+                                        }}
+                                        options={{
+                                            preset: `islands#${index === this.state.selectedPoint ? 'blue' : 'grey'}StretchyIcon`,
+                                        }}
+                                    />
+                                    <GeoObject 
+                                        geometry={{
+                                            type: 'Circle',
+                                            coordinates: [55.7, 37.6],
+                                            radius: marker.radius
+                                        }}
+                                    />
+                                </>
                             ))}
                     </Map>
                 </YMaps>
